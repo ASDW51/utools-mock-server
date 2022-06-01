3357f0aba995396d5006c3c286fe391bc845d488
ASDW51 <1598926913@qq.com>
2022-06-01 17:29:59 +0800

    add flatPath util

---
+++ src/util/index.js
@@ -0,0 +1,36 @@
+/*
+let project = {
+    "test1":{
+        name:"测试项目",
+        path:"/",
+        editable:false,
+        type:"dir",
+        params:{},
+        children:[
+            {
+                name:"用户",
+                path:"user",
+                editable:false,
+                type:"item",
+                params:{},
+                format:{}
+            },
+        ]
+    }
+}
+*/
+const flatPath = (arr,path,target)=>{
+    arr.children && arr.children.map(item=>{
+      if(item.children && item.children.length > 0 && item.type == 'dir'){
+          d(item,path + item.path + '/',target)
+      }else{
+          let p = path+item.path
+         item.type=='item' && target.push({...item,path:p})
+      }
+    })
+}
+
+export {flatPath}
+export default {
+    flatPath
+}
